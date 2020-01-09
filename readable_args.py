e68ae2231d2ca662f855c7f15d6d1bbcd8ef3aca
pu1p <pullp@foxmail.com>
2020-01-09 10:41:52 +0800

    feat: readable args for iofile-flags

--- readable_args.py
+++ readable_args.py
@@ -1,3 +1,55 @@
+#coding:utf-8
+from ast import literal_eval
+
+def _get_args(s):
+    if "("  in s:
+        s = s[s.find("(")+1:]
+    if ")" in s:
+        s = s[:s.find(")")]
+    return s.split(",")
+
+def iofile_flags(flag):
+    if type(flag) == str:
+        flag = literal_eval(flag)
+    # from glibc 2.23
+    defines = {'_IO_USER_BUF' : 1,
+        '_IO_UNBUFFERED' : 2,
+        '_IO_NO_READS' : 4,
+        '_IO_NO_WRITES' : 8,
+        '_IO_EOF_SEEN' : 0x10,
+        '_IO_ERR_SEEN' : 0x20,
+        '_IO_DELETE_DONT_CLOSE' : 0x40,
+        '_IO_LINKED' : 0x80,
+        '_IO_IN_BACKUP' : 0x100,
+        '_IO_LINE_BUF' : 0x200,
+        '_IO_TIED_PUT_GET' : 0x400,
+        '_IO_CURRENTLY_PUTTING' : 0x800,
+        '_IO_IS_APPENDING' : 0x1000,
+        '_IO_IS_FILEBUF' : 0x2000,
+        '_IO_BAD_SEEN' : 0x4000,
+        '_IO_USER_LOCK' : 0x8000,
+    }
+    flag_res = ""
+    for d in defines:
+        if (flag & defines[d]) != 0:
+            flag_res += (d + " | ")
+    flag_res = flag_res[:-2]
+    print("iofile._flags is : "+flag_res)
+
+def clone(s):
+    args = _get_args(s)
+    fn = args[0]
+    child_stack = args[1]
+    flags = args[2]
+    rest_args = ", ".join(args[3:])
+    #todo
+
+def socket(s):
+    # int socket(int domain, int type, int protocol);
+    args = _get_args(s)
+
+
+# finished
 def mmap(s):
     def show_prot(prot_int):
         # from glibc-2.23 mman.h
@@ -42,11 +94,7 @@ def mmap(s):
         flag_res = flag_res[:-2]
         print("flag is : "+flag_res)
 
-    if "("  in s:
-        s = s[s.find("(")+1:]
-    if ")" in s:
-        s = s[:s.find(")")]
-    args = s.split(",")
+    args = _get_args(s)
     addr = args[0]
     length = args[1]
     prot = eval(args[2])
@@ -60,4 +108,9 @@ def mmap(s):
     print("fd is : "+fd)
     print("offset is : "+offset)
     
+
+
+
+
+
 
